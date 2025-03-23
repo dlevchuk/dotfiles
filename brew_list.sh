c9e88cd25899ac280ded8643caf64b43d45b4d3a
dlevchuk <lastdreamer17@gmail.com>
2025-03-23 14:21:41 +0200

    Update brew_list.sh

--- brew_list.sh
+++ brew_list.sh
@@ -12,7 +12,7 @@ brew upgrade
 
 # Install formulae
 formulae=(
-  atuin awscli bash-completion bat brotli c-ares ca-certificates cairo cask cffi coreutils cryptography diffutils docker docker-completion docker-compose duf emacs eza flux fontconfig freetype gdbm gettext gh giflib glib gmp gnutls gping graphite2 gsl harfbuzz helm htop icu4c@75 icu4c@76 jansson jpeg-turbo jq kubernetes-cli libevent libgit2 libgit2@1.7 libidn2 liblinear libnghttp2 libpng libssh2 libtasn1 libtiff libtommath libunistring libuv libx11 libxau libxcb libxdmcp libxext libxrender little-cms2 lua lz4 lzo maven midnight-commander mpdecimal mtr ncurses nettle nmap node nvm oniguruma openjdk openssl@3 p11-kit p7zip pcre pcre2 pixman powerlevel10k pycparser python-packaging python@3.8 python@3.11 python@3.12 python@3.13 readline redis ripgrep s-lang sqlite tailspin tcl-tk telnet terraform terragrunt tree tree-sitter unbound wget xorgproto xz zsh zsh-autosuggestions zsh-syntax-highlighting zstd
+  atuin awscli bash-completion bat docker docker-completion docker-compose duf eza flux gping helm htop jq kubernetes-cli midnight-commander mtr nmap powerlevel10k ripgrep  tailspin telnet terraform terragrunt tree  wget  zsh zsh-autosuggestions zsh-syntax-highlighting 
 )
 
 for formula in "${formulae[@]}"; do
@@ -21,8 +21,10 @@ done
 
 # Install casks
 casks=(
-  alt-tab appcleaner caffeine coconutbattery dbeaver-community discord google-chrome iterm2 karabiner-elements latest lens multipass notion obsidian raycast shottr sloth sublime-text telegram visual-studio-code vlc windterm
+  appcleaner coconutbattery dbeaver-community discord google-chrome iterm2 karabiner-elements lens obsidian raycast shottr sloth telegram visual-studio-code vlc 
 )
+
+#alt-tab caffeine multipass notion sublime-text windterm
 
 for cask in "${casks[@]}"; do
   brew install --cask "$cask"
